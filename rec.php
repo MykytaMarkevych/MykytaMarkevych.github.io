19e5790a260a90b86964e7d3dbaed673c7fb83d7
MykytaMarkevych <47721959+MykytaMarkevych@users.noreply.github.com>
2020-01-04 02:09:25 +0200

    Update rec.php

--- rec.php
+++ rec.php
@@ -1,12 +1,13 @@
-<?php 
+<html>
+ <head>
+  <title>Тестируем PHP</title>
+ </head>
+ <body>
 
-$db = new SQLite3('t.db');
-
-$res = $db->query('SELECT * FROM t');
-
-while ($row = $res->fetchArray()) {
-    echo "{$row[1]} {$row[2]} {$row[3]} \n";
-}
-
+ <?php 
+	echo '<p>Привет, мир!</p>'; 
 ?>
+
+ </body>
+</html>
 
